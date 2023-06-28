f0c4becd5d578c05434e206fec9ca5e949fffbaa
Peter <faffyroypeter@live.com>
2023-06-28 23:38:06 +0530

    remvoed key

--- CE.CheckBalance/FuncBlobTrigger.cs
+++ CE.CheckBalance/FuncBlobTrigger.cs
@@ -9,7 +9,7 @@ namespace CE.CheckBalance
     public class FuncBlobTrigger
     {
         [FunctionName("FuncBlobTrigger")]
-        public void Run([BlobTrigger("customer-pictures/{name}", Connection = "DefaultEndpointsProtocol=https;AccountName=stroageaccountnafinterns;AccountKey=5K3zoBHF2ySHBW+sF1HBei/zBxriV2Q8vlrErLjDOXlRVfdY9vjXKKQLPSQ4Ix82HLqzzMQ8fnE4+AStqDJDSA==;EndpointSuffix=core.windows.net")] Stream myBlob, string name, ILogger log)
+        public void Run([BlobTrigger("customer-pictures/{name}", Connection = "")] Stream myBlob, string name, ILogger log)
         {
             log.LogInformation($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {myBlob.Length} Bytes");
         }
